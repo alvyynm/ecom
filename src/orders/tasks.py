b92f0dd875bf7efe11ee39858fc1630241685e24
alvyynm <alvyynm@gmail.com>
2024-11-20 06:46:17 +0300

    feat: create celery task for sending email after order placement

---
+++ src/orders/tasks.py
@@ -0,0 +1,26 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from decouple import config
+
+from .models import Order
+
+ADMIN_EMAIL = config('SHOP_ADMIN_EMAIL')
+
+
+@shared_task
+def order_created(order_id):
+    """
+    Task to send an e-mail notification when an order is
+    successfully created.
+    """
+    order = Order.objects.get(id=order_id)
+    subject = f'Order nr. {order.id}'
+    message = (
+        f'Dear {order.first_name},\n\n'
+        f'You have successfully placed an order.'
+        f'Your order ID is {order.id}.'
+    )
+    mail_sent = send_mail(
+        subject, message, ADMIN_EMAIL, [order.email]
+    )
+    return mail_sent
