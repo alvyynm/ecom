51a305b4ef0dc6bb066fa9d86d363ef38406103d
alvyynm <alvyynm@gmail.com>
2024-11-20 08:34:23 +0300

    feat: create success and canceled views

--- src/payments/views.py
+++ src/payments/views.py
@@ -45,4 +45,12 @@ def payment_process(request):
         # redirect to Stripe payment form
         return redirect(session.url, code=303)
     else:
-        return render(request, 'payment/process.html', locals())
+        return render(request, 'payments/process.html', locals())
+
+
+def payment_completed(request):
+    return render(request, 'payments/completed.html')
+
+
+def payment_canceled(request):
+    return render(request, 'payments/canceled.html')
