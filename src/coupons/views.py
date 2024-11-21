5747025253a7b0cbca092659da61c56103a6a2b1
alvyynm <alvyynm@gmail.com>
2024-11-21 07:47:29 +0300

    feat: create coupon view and urls

--- src/coupons/views.py
+++ src/coupons/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.views.decorators.http import require_POST
 
+from .forms import CouponApplyForm
+from .models import Coupon
 # Create your views here.
+
+
+@require_POST
+def coupon_apply(request):
+    now = timezone.now()
+    form = CouponApplyForm(request.POST)
+    if form.is_valid():
+        coupon_code = form.cleaned_data['coupon_code']
+        try:
+            # check for valid coupon code
+            coupon = Coupon.objects.get(code__iexact=coupon_code,
+                                        valid_from__lte=now,
+                                        valid_to__gte=now,
+                                        active=True)
+            # store the coupon id in user session
+            request.session['coupon_id'] = coupon.id
+        except Coupon.DoesNotExist:
+            # if it doesn't exist, store None
+            request.session['coupon_id'] = None
+        return redirect('cart:cart_detail')
