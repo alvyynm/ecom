9ffa47feaeb37a6667f86b36b5926625eef75e25
alvyynm <alvyynm@gmail.com>
2024-11-28 08:01:34 +0300

    feat: create user registration functionality

--- src/authentication/forms.py
+++ src/authentication/forms.py
@@ -5,3 +5,11 @@ class LoginForm(forms.Form):
     """Login form"""
     username = forms.CharField(max_length=80)
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class RegisterForm(forms.Form):
+    """Register form"""
+    username = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirmation = forms.CharField(widget=forms.PasswordInput)
--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, "Signup successful. Please login.")
+            return redirect('authentication:login')
+        else:
+            messages.error(request, "An error occurred while signing you up.")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html",
+                  {'form': form})
