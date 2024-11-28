9e671fadd9f199057ba6ee4177ac0c699d05d291
alvyynm <alvyynm@gmail.com>
2024-11-28 13:27:42 +0300

    feat: create custom profile

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    class Gender(models.TextChoices):
+        MALE = 'male'
+        FEMALE = 'female'
+        NULL = 'none'
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone = models.PositiveIntegerField(null=True, blank=True)
+    gender = models.CharField(max_length=6, choices=Gender.choices)
+    date_of_birth = models.DateField(null=True, blank=True)
+    home_address = models.CharField(max_length=250, null=True, blank=True)
+    delivery_address = models.CharField(max_length=250, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
