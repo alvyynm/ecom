995b67f8ff36a07066407ce0f748f4cfb65c2e4e
alvyynm <alvyynm@gmail.com>
2024-11-28 13:32:52 +0300

    feat: add profile model to admin

--- src/authentication/admin.py
+++ src/authentication/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone', 'gender', 'date_of_birth',
+                    'home_address', 'delivery_address', 'created', 'updated']
+    list_filter = ['gender', 'created', 'updated']
+    raw_id_fields = ['user']
