675507fcb7a320f12305e99ef6d4640653938746
alvyynm <alvyynm@gmail.com>
2024-11-29 07:15:18 +0300

    feat: add email login

--- src/project/settings/base.py
+++ src/project/settings/base.py
@@ -161,3 +161,9 @@ CELERY_TASK_SERIALIZER = 'json'
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Custom authentication backend
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'authentication.authentication.EmailAuthBackend',
+]
