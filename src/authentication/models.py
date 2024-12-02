92d9a94e435a653e97532db8adb135bced3af0cc
alvyynm <alvyynm@gmail.com>
2024-12-02 11:25:55 +0300

    feat: add city and postal_code to Profile model

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -15,6 +15,8 @@ class Profile(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     home_address = models.CharField(max_length=250, null=True, blank=True)
     delivery_address = models.CharField(max_length=250, null=True, blank=True)
+    postal_code = models.CharField(max_length=20, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
