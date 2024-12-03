c0c8c662e4b7e8010a942fe44a9320711092a850
alvyynm <alvyynm@gmail.com>
2024-12-03 09:42:59 +0300

    feat: style add to cart btn in product detail page

--- src/cart/forms.py
+++ src/cart/forms.py
@@ -8,3 +8,11 @@ class CartAddProductForm(forms.Form):
         choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
     override = forms.BooleanField(
         required=False, initial=False, widget=forms.HiddenInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Add these classes to all form fields
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+            })
